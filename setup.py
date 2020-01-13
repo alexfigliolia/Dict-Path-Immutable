0fe626052fac03877143406addcd1fe3f2c235ed
Alex <alex@carta.healthcare>
2020-01-12 17:38:55 -0800

    Fix typo

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import setuptools
+
 with open("README.md", "r") as fh:
   long_description = fh.read()
+  
 setuptools.setup(
   name='Object_Path_Immutable',  
   version='0.1',
@@ -8,6 +10,7 @@ setuptools.setup(
   author="Alex Figliolia",
   author_email="alexfigliolia@gmail.com",
   description="Modify deep object properties without modifying the original object (immutability)",
+  long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/alexfigliolia/Object-Path-Immutable-Python",
   packages=setuptools.find_packages(),
